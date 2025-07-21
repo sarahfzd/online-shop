487e4f5f246a835af82cf5a194239df28ac575bf
SarahFzd <fzd.sara@gmail.com>
2025-07-21 14:58:00 +0330

    add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HeaderBar from './components/HeaderBar.vue'
 import { RouterView } from 'vue-router'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
@@ -8,4 +9,5 @@ import { RouterView } from 'vue-router'
   <main class="container-fluid col-lg-11 mx-auto">
     <RouterView />
   </main>
+  <Footer />
 </template>

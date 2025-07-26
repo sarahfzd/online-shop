7e8a04263912e808f8955f08e6183c9af459ae4c
SarahFzd <fzd.sara@gmail.com>
2025-07-26 15:18:59 +0330

    search queries

--- src/pages/ShopPage.vue
+++ src/pages/ShopPage.vue
@@ -1,11 +1,41 @@
 <script setup>
-import { onMounted } from 'vue'
+import { watch, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import { useProductsStore } from '../stores/products'
+import qs from 'qs'
 import Filters from '../components/Filters.vue'
 import ProductList from '../components/ProductList.vue'
 
+const route = useRoute()
+const router = useRouter()
 const store = useProductsStore()
-onMounted(store.fetchProducts)
+onMounted(() => {
+    const query = route.query
+
+    if (query.color) {
+        store.selectedColors = Array.isArray(query.color) ? query.color : [query.color]
+    }
+    if (query.size) {
+        store.selectedSizes = Array.isArray(query.size) ? query.size : [query.size]
+    }
+    if (query.available === 'true') {
+        store.onlyAvailable = true
+    }
+
+    store.fetchProducts()
+})
+watch(
+    [() => store.selectedColors, () => store.selectedSizes, () => store.onlyAvailable],
+    () => {
+        const query = {
+            ...(store.selectedColors.length && { color: store.selectedColors }),
+            ...(store.selectedSizes.length && { size: store.selectedSizes }),
+            ...(store.onlyAvailable && { available: 'true' })
+        }
+        router.replace({ path: route.path, query })
+    },
+    { deep: true }
+)
 </script>
 
 <template>
